fd255d2a61e13c1eb17ee48f2a77bd0cdf861d4f
Brayan-Guilherme <161321907+Brayan-Guilherme@users.noreply.github.com>
2024-11-11 07:52:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,57 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'curiosidades',
+    'qual é o animal mais venenoso do mundo?',
+    'é a agua-viva caixa (cubozoa)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+)
+
+criaCartao(
+    'astronomia',
+    'qual a maior estrela do universo?',
+    'é a VY Canis Majoris também conhecida como VY Cma sendo 2.100 vezes maior que o sol em diametro'
+)
+
+
+criaCartao(
+    'curiosidade',
+    'qual maior orgão do corpo humano?',
+    'a pele'
+)
+
+
+criaCartao(
+    'curiosodade',
+    'qual o livro mais vendido do mundo?',
+    'a bíblia com mais de 5 bilhões de cópias vendidas e distribuidas'
+)
+
+
+criaCartao(
+    'comida',
+    'qual a fruta mais saudável do mundo?',
+    'o limão siciliano'
+)
+
